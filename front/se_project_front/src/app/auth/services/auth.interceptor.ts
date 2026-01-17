15fa85a74a8e6061a1dc613652c6365ee7b59209
matisdbs37 <matisdubois18@gmail.com>
2026-01-17 19:10:52 +0100

    Remove console logs

--- front/se_project_front/src/app/auth/services/auth.interceptor.ts
+++ front/se_project_front/src/app/auth/services/auth.interceptor.ts
@@ -5,10 +5,8 @@ import { OAuthService } from 'angular-oauth2-oidc';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const oauthService = inject(OAuthService);
   const token = oauthService.getIdToken(); 
-  console.log("INTERCEPTOR - URL:", req.url, "TOKEN PRESENT ?:", !!token);
-  console.log("token :", token);
+
   if (token && req.url.includes('localhost:8080')) {
-    console.log("INTERCEPTOR - Ajout du token pour le backend");
     const authReq = req.clone({
       setHeaders: { Authorization: `Bearer ${token}` }
     });
